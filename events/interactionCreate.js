b375867af9bdf52e9807199e3e0e8529bf00bd3a
matias.henriquez <matias.henriquez@puentemaipo.cl>
2025-07-16 05:49:06 +0000

    create event interactionCreate

---
+++ events/interactionCreate.js
@@ -0,0 +1,21 @@
+const { Events } = require("discord.js");
+
+module.exports = {
+  name: Events.InteractionCreate,
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand()) return;
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+      await interaction.reply(
+        `The command ${interaction.commandName} don't exists, try use /help`
+      );
+    } else {
+      try {
+        await command.execute(interaction);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+};
